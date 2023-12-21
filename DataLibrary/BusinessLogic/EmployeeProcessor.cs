bfa041634a9b8ca42bdc454eeb85b69ada8c8f2e
lifeface360 <lifeface360@gmail.com>
2023-12-21 08:21:49 +0800

    加入專案檔案。

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,39 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System.Collections.Generic;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class EmployeeProcessor
+    {
+        public static int CreateEmployee(int employeeId, string firstName, 
+            string lastName, string emailAddress)
+        {
+            EmployeeModel data = new EmployeeModel
+            {
+                EmployeeId = employeeId,
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress
+
+            };
+            
+            
+            string sql = @"insert into dbo.Employee (EmployeeId, FirstName, LastName, EmailAddress)
+                            values(@EmployeeId, @FirstName, @LastName, @EmailAddress);";
+            // These are parameterized SQL statements. The '@' symbol indicates placeholders for parameter values.
+            // They are placeholders where actual data will be passed along with the SQL statement.
+
+            // Map our model to the corresponding parameters in the SQL query and execute the query.
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = @"select Id, EmployeeId, FirstName, LastName, EmailAddress
+                            from dbo.Employee;";
+
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+    }
+}
---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace DataLibrary.DataAccess
+{
+    public static class SqlDataAccess
+    {
+        public static string GetConnectionString(string connectionName = "MVCDemoDB")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+
+        // we return int, cuz that's a number of rows affected so we have something to return
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+    }
+}
